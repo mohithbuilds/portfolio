a9cdf4f2e70227a6997e3ab21c9e24214c0e9ceb
mohithn04 <mohith.n2022@gmail.com>
2025-11-21 00:02:22 -0800

    (feat): add separate contact page

--- apps/portfolio/src/routes/contact/+page.svelte
+++ apps/portfolio/src/routes/contact/+page.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import ContactForm from '@/components/ContactForm.svelte';
+	import type { PageProps } from './$types';
 
-	let { data }: { data: PageData } = $props();
+	let { data }: PageProps = $props();
+	let contactForm = $derived(data.contactForm);
 </script>
+
+<div class="mx-auto max-w-7xl w-full">
+    <section>
+        <h2 class="pb-4 text-3xl font-bold pt-8">Contact</h2>
+        <ContactForm {contactForm} />
+    </section>
+</div>
