2ddc1fd793254733c55c37cb28f4e77a17f5e35f
mohithn04 <mohith.n2022@gmail.com>
2025-11-24 02:27:13 -0800

    (chore): clean up unnecessary images

--- apps/portfolio/src/components/Socials.svelte
+++ apps/portfolio/src/components/Socials.svelte
@@ -8,10 +8,10 @@
 
 {#if !isBlogPost}
 	<meta property="og:site_name" content="Mohith Nagendra" />
-	<meta property="og:url" content="https://kyrre.dev" />
+	<meta property="og:url" content="https://mohithn.vercel.app" />
 	<meta property="og:type" content="website" />
 	<meta property="og:title" content="Mohith Nagendra - Full-stack developer" />
-	<meta property="og:image" content="https://kyrre.dev/images/og-kyrre-gjerstad.jpg" />
+	<meta property="og:image" content="https://mohithn.vercel.app/images/og-mohith-nagendra.jpg" />
 	<meta
 		property="og:description"
 		content="Portfolio of Mohith Nagendra, a full-stack developer specializing in NextJS and SvelteKit."
@@ -19,12 +19,12 @@
 	<meta property="og:image:width" content="1200" />
 	<meta property="og:image:height" content="630" />
 	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:site" content="@kyrregjerstad" />
-	<meta name="twitter:creator" content="@kyrregjerstad" />
+	<meta name="twitter:site" content="@mohithn04" />
+	<meta name="twitter:creator" content="@mohithn04" />
 	<meta name="twitter:title" content="Mohith Nagendra - Full-stack developer" />
 	<meta
 		name="twitter:description"
 		content="Portfolio of Mohith Nagendra, a full-stack developer specializing in NextJS and SvelteKit."
 	/>
-	<meta name="twitter:image" content="https://kyrre.dev/images/og-kyrre-gjerstad.jpg" />
+	<meta name="twitter:image" content="https://mohithn.vercel.app/images/og-mohith-nagendra.jpg" />
 {/if}
